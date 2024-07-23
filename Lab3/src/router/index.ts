461de6ab6911e9bf934354fe01d6bac442f001bf
Bambam <bambam.pataranan@gmail.com>
2024-07-23 16:34:54 +0700

    Change to PassengerListView

--- Lab3/src/router/index.ts
+++ Lab3/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PassengerListView from '../views/PassengerListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'passenger-list-view',
+      component: PassengerListView
     },
     {
       path: '/about',
