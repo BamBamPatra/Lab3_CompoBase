5f398ad0d07f59755dd698302ae805301ee2b793
Bambam <bambam.pataranan@gmail.com>
2024-07-25 16:55:43 +0700

    use useRouter function
    routing to event detail

--- Lab3/src/views/passenger/RegisterView.vue
+++ Lab3/src/views/passenger/RegisterView.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-import { toRefs,defineProps } from 'vue'
+import { toRefs,defineProps, useAttrs } from 'vue'
 import { type Passenger } from '@/types'
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
     passenger: Passenger
     id: String
 }>()
 const { passenger } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    //if the registration API call successful
+    //Push back to thr passenger details view
+    router.push({ name: 'passenger-detail-view'})
+}
 
 </script>
 
 <template>
     <p>Register passenger here</p>
+    <button @click="register">Register</button>
 </template>
