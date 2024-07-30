2fecb04f16920d6ce89330f5e19750a4b8981eed
Bambam <bambam.pataranan@gmail.com>
2024-07-30 23:48:53 +0700

    Click back button return to same page same position

--- Lab3/src/router/index.ts
+++ Lab3/src/router/index.ts
@@ -79,7 +79,14 @@ const router = createRouter({
       name: 'not-found',
       component: NotFoundView
     }
-  ]
+  ],
+  scrollBehavior(to , from , savedPosition) {
+    if (savedPosition){
+      return savedPosition
+    }else {
+    return { top: 0 }
+    }
+  }
 })
 
 router.beforeEach(() => {
--- Lab3/src/views/PassengerListView.vue
+++ Lab3/src/views/PassengerListView.vue
@@ -14,13 +14,13 @@ const props = defineProps({
 const page = computed(() => props.page)
 const totalPassengers = ref(0)
 const hasNextPage = computed(() => {
-  const totalPages = Math.ceil(totalPassengers.value / 2)
+  const totalPages = Math.ceil(totalPassengers.value / 3)
   return page.value < totalPages
 })
 
 onMounted(() => {
   watchEffect(() => {
-    PassengerService.getPassengers(2, page.value)
+    PassengerService.getPassengers(3, page.value)
     .then((response) => {
       console.log(response.data);
       passengers.value = response.data.data; 
